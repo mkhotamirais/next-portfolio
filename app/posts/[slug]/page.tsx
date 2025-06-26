b80687b8ac4e1600f5ab32135fe99d987283264c
mkhotamirais <mkhotamirais@gmail.com>
2025-06-26 09:13:31 +0700

    berhasil

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -47,7 +47,7 @@ export default async function PostPage({ params }: { params: Promise<{ slug: str
   const html = marked(post.content, { renderer });
 
   return (
-    <section className="section scroll-mt-18">
+    <section className="py-4">
       <div className="container">
         <article className="prose dark:prose-invert" dangerouslySetInnerHTML={{ __html: html }}></article>
       </div>
